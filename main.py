bff3bab599b76bc3688eec373e168b8a9579c836
Armel Tchomnou <89040665+Rayarmel@users.noreply.github.com>
2022-08-18 23:11:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,11 @@ import argparse
 import scapy.layers.l2 as scapy2
 import scapy.sendrecv as scapy3
 # use to format input correctly by creating regular expressions
-# import re
 parser = argparse.ArgumentParser()
 # need root/admin privileges to call from command line
 parser.add_argument("-ip", "--ipadd", help="IP Address/Subnet Mask")
 args = parser.parse_args()
-# regular expression pattern for IPv4 address
-# ipPattern = re.compile("^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}/[0-9]*$")
+
 if not args.ipadd:
     print("Invalid Syntax \n Use --help or -h for options")
     sys.exit(1)
@@ -32,14 +30,5 @@ else:
 # hwsrc = destination mac addr
     for i in range(len(result)):
         print(result[i])
-# # address range to ARP
-# while True:
-#     ipEntered = input("\n Enter ip address and range you want to send ARP request to (ex: 255.255.255.0/24)")
-#     if ipPattern.search(ipEntered):
-#         print(f"{ipEntered} is a valid ip address range")
-#         break
-#
-# # ARP ip address range given
-# # If valid, a list of all results will be returned
-#
-# arpResult = arping(ipEntered)
+# ARP ip address range given
+# If valid, a list of all results will be returned
